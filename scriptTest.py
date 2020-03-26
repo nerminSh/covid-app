4c4699afd4575f3e077650e306859b6978d592bb
nerminSh <nermin.shero1@gmail.com>
2020-03-26 23:25:54 +0100

    carousel new images and slide fixed bug

--- scriptTest.py
+++ scriptTest.py
@@ -5,12 +5,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("indexx.html")
 
 @app.route('/home')
 def home():
     return render_template("home.html")
-
 
 
 @app.route('/map/')
