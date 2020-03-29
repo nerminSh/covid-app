e21925a024f521cce584ecdc02a2aa8bcedadbab
nerminSh <nermin.shero1@gmail.com>
2020-03-29 23:28:55 +0200

    Form added

--- scriptTest.py
+++ scriptTest.py
@@ -1,17 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import app2 as mapa
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template("indexx.html")
+    return render_template("index.html")
 
-@app.route('/home')
-def home():
-    return render_template("home.html")
+@app.route('/stayAtHome')
+def forma():
+    return render_template("forma.html")
 
+@app.route('/result', methods = ['POST', 'GET'])
+def result():
+    if request.method == 'POST':
+        name = request.form["firstName"]
+        rolls = request.form["rolls"]
+        soc = request.form["socOpt"]
+        rolls = int(rolls)
+        if name.capitalize() in ("Nedim", "Ibrahim", "Mugdim", "Ahmed", "Ekrem", "Ibro", "Anes", "Dzenan"):
+            perc = name.capitalize() + "e ti si jedan veliki covIDIOT heheh salim se\n btw kupi jos toalet papira. Selam"
+        elif rolls > 30 and soc == "socNo":
+            perc = "You are definitely a covIDIOT."
+        elif rolls <= 30 and soc == "socYes":
+            perc = "Good job, you are not a covIDIOT"
+        else:
+            perc = "You are almost a covIDIOT, let's say you're halfway there"
 
+        return render_template("result.html", firstname = name.capitalize(), perc = perc)
+
+ 
 @app.route('/map/')
 def map():
     return mapa.map._repr_html_()
